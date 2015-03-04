b79794a3947b6e9ea1c29c096948e98c5146d8e8
dmitry-ananchenko <di.uspeh@gmail.com>
2015-03-04 20:53:10 +0200

    First Piano commit

---
+++ Assets/scripts/PianoKey.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PianoKey : MonoBehaviour {
+
+	// public float semitone_offset = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown() {
+		// audio.pitch = Mathf.Pow( 2f, semitone_offset/12.0f );
+		audio.Play ();
+	}
+}

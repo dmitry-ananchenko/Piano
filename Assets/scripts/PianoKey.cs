00c57ebff88e810e891c0e1f2c5fb31e92f99b9e
dmitry-ananchenko <di.uspeh@gmail.com>
2015-03-09 18:20:43 +0200

    group obj

--- Assets/scripts/PianoKey.cs
+++ Assets/scripts/PianoKey.cs
@@ -7,7 +7,8 @@ public class PianoKey : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		GameObject.Find("black_key").GetComponent<Renderer>().material.color = Color.black;
+		// GetComponent<Renderer>().material.color = Color.black;
 	}
 	
 	// Update is called once per frame
@@ -19,10 +20,10 @@ public class PianoKey : MonoBehaviour {
 		// audio.pitch = Mathf.Pow( 2f, semitone_offset/12.0f );
 		GetComponent<AudioSource>().Play ();
 		// GetComponent<Rigidbody>().RotateAround (Vector3.zero, Vector3.up, 20 * Time.deltaTime);
-		GetComponent<Rigidbody>().rotation = Quaternion.Euler ( -5.0f, 0.0f, 0.0f);
+		GetComponent<Transform>().rotation = Quaternion.Euler ( -5.0f, 0.0f, 0.0f);
 	}
 
 	void OnMouseUp () {
-		GetComponent<Rigidbody>().rotation = Quaternion.Euler ( 0.0f, 0.0f, 0.0f);
+		GetComponent<Transform>().rotation = Quaternion.Euler ( 0.0f, 0.0f, 0.0f);
 	}
 }
